b769ca0cf9ed49145619d59a66b518d3915df033
Simo Naatula <git@naatu.la>
2021-04-24 11:42:14 +0300

    Remove bcrypt

--- deps.js
+++ deps.js
@@ -2,6 +2,3 @@ export { Application, Router, send } from "https://deno.land/x/oak@v6.5.0/mod.ts
 export { Session } from "https://deno.land/x/session@v1.0.0/mod.ts"
 export { viewEngine, engineFactory, adapterFactory } from "https://deno.land/x/view_engine@v1.4.5/mod.ts"
 export { DB } from "https://deno.land/x/sqlite@v2.3.2/mod.ts"
-
-import * as bcrypt from "https://deno.land/x/bcrypt@v0.2.4/mod.ts"
-export { bcrypt }
